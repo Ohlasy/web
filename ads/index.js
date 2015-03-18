5d5dd7150adc1b0adc28cc937b47923bbe0c4e43
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-03-18 08:09:08 +0100

    Přidal jsem podporu pro rotující bannery a banner Lísek.

---
+++ ads/index.js
@@ -0,0 +1,13 @@
+[
+    {
+        "name": "Lísky březen",
+        "image": "/ads/lisky.png",
+        "alt": "Improvizační zápas Lísky, 27. března"
+    },
+    {
+        "name": "Inzerujte u nás",
+        "image": "/ads/generic.png",
+        "alt": "Inzerujte u nás! ohlasy@ohlasy.info",
+        "url": "mailto:ohlasy@ohlasy.info"
+    }
+]
