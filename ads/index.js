8a248fbaa0660aaca0df67f5cb779b1a7f2d0bb9
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-03-26 08:22:02 +0100

    Přidal jsem banner na Stabat mater.

--- ads/index.js
+++ ads/index.js
@@ -6,6 +6,12 @@
         "url": "https://www.facebook.com/pages/L%C3%ADsky-boskovický-improvizačn%C3%AD-tým/510533395668618"
     },
     {
+        "name": "Stabat Mater 28/3",
+        "image": "/ads/stabat.png",
+        "alt": "Koncert: Stabat mater, 28. března v Kunštátě",
+        "url": "http://vernum.cz"
+    },
+    {
         "name": "Inzerujte u nás",
         "image": "/ads/generic.png",
         "alt": "Inzerujte u nás! ohlasy@ohlasy.info",
