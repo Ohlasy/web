289b71aad6c4bb3d5770cfb7e7f9bf6013c60fd3
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-04-06 17:40:30 +0200

    Reklama Nature Home.

--- ads/index.js
+++ ads/index.js
@@ -7,11 +7,18 @@
         "priority": 1000
     },
     {
+        "name": "Nature Home",
+        "image": "/ads/nature-home.jpg",
+        "alt": "Nature Home, interiér pro váš domov i kancelář",
+        "url": "http://www.naturehome.cz/",
+        "priority": 1000
+    },
+    {
         "name": "Michal Prokop",
         "image": "http://kulturaboskovice.cz/img/ohlasy.jpg",
         "alt": "reklama na aktuální koncerty KZMB",
         "url": "http://kulturaboskovice.cz/",
-        "priority": 1000
+        "priority": 900
     },
     {
         "name": "Inzerujte u nás",
