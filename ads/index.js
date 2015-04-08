b00a2261513ed5a63406e1a731205ed7d1265247
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2015-04-08 09:28:35 +0200

    Aktualizoval jsem reklamní bannery.

--- ads/index.js
+++ ads/index.js
@@ -1,12 +1,5 @@
 [
     {
-        "name": "Mike DiRubbo Trio",
-        "image": "/ads/mike.jpg",
-        "alt": "Mike DiRubbo Trio, sál ZUŠ Boskovice, 7. dubna",
-        "url": "http://kulturaboskovice.cz/",
-        "priority": 1000
-    },
-    {
         "name": "Nature Home",
         "image": "/ads/nature-home.jpg",
         "alt": "Nature Home, interiér pro váš domov i kancelář",
@@ -14,11 +7,11 @@
         "priority": 1000
     },
     {
-        "name": "Michal Prokop",
+        "name": "KZMB",
         "image": "http://kulturaboskovice.cz/img/ohlasy.jpg",
-        "alt": "reklama na aktuální koncerty KZMB",
+        "alt": "reklama na aktuální koncert KZMB",
         "url": "http://kulturaboskovice.cz/",
-        "priority": 900
+        "priority": 1000
     },
     {
         "name": "Inzerujte u nás",
