4b9947277f7b61cb5de2875ba9bd05dee8f8fe3d
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2016-01-16 20:50:46 +0100

    Přidal jsem do API článků náhledové fotky.

--- api/articles.js
+++ api/articles.js
@@ -9,6 +9,7 @@ why: A dummy comment to make Jekyll process this file
         "author": {{ article.author | jsonify }},
         "category": {{ article.category | jsonify }},
         "pubDate": {{ article.date | jsonify }},
+        "cover-photo": {{ article.cover-photo | jsonify }},
         "perex": {{ article.excerpt | strip_html | jsonify }},
         "serial": {{ article.serial | jsonify }},
         "relativeURL": {{ article.url | jsonify }}
