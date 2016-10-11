238e60d821b102a30a944e20f8e69b111cb0434c
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2016-10-11 21:50:48 +0200

    Počítání nejčtenějších článků za poslední dva měsíce

--- _bin/top-articles.hs
+++ _bin/top-articles.hs
@@ -6,7 +6,7 @@ import Network.HTTP.Types (hAuthorization)
 import Data.Monoid
 import qualified Data.ByteString.Char8 as B8
 
-topReadArticlesQueryURL = "https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A97173197&start-date=30daysAgo&end-date=yesterday&metrics=ga%3Ausers%2Cga%3Apageviews&dimensions=ga%3ApagePath%2Cga%3ApageTitle&sort=-ga%3Apageviews&filters=ga%3ApagePath%3D%40%2Fclanky%2F20&max-results=10"
+topReadArticlesQueryURL = "https://www.googleapis.com/analytics/v3/data/ga?ids=ga%3A97173197&start-date=60daysAgo&end-date=yesterday&metrics=ga%3Ausers%2Cga%3Apageviews&dimensions=ga%3ApagePath%2Cga%3ApageTitle&sort=-ga%3Apageviews&filters=ga%3ApagePath%3D%40%2Fclanky%2F20&max-results=10"
 
 main :: IO ()
 main = do
