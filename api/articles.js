d35264df861ea5bf681a7835d5c5e711df7bd4ec
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2019-03-27 18:04:19 +0100

    Přidal jsem do JSON feedu článků seznam tagů

--- api/articles.js
+++ api/articles.js
@@ -12,7 +12,8 @@ why: A dummy comment to make Jekyll process this file
         "cover-photo": {{ article.cover-photo | jsonify }},
         "perex": {{ article.excerpt | strip_html | jsonify }},
         "serial": {{ article.serial | jsonify }},
-        "relativeURL": {{ article.url | jsonify }}
+        "relativeURL": {{ article.url | jsonify }},
+        "tags": {{ article.tags | jsonify }}
     }
     {% if forloop.last == false %},{% endif %}
 {% endfor %}
