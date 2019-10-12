cb7d828fab4dddb927509a434173ff5f20ed80a1
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2019-10-12 17:26:12 +0200

    Nevynucujme si všechny obrázky jako JPEG (#7)

--- _plugins/imgproxy_sign.rb
+++ _plugins/imgproxy_sign.rb
@@ -20,7 +20,7 @@ end
 def signed_thumb_url(src_url, width)
 
     encoded_url = Base64.urlsafe_encode64(src_url).tr("=", "").scan(/.{1,16}/).join("")
-    path = "/fit/#{width}/9999/sm/0/#{encoded_url}.jpg"
+    path = "/fit/#{width}/9999/sm/0/#{encoded_url}"
     hmac = sign_path(path)
 
     return "https://thumbs-cdn.ohlasy.info/#{hmac}#{path}"
