97255491eddc38185bc713a23361fdfdd7bf9cf2
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2020-03-27 08:42:48 +0100

    Zmenšení náhledových fotek v API

--- api/articles.js
+++ api/articles.js
@@ -9,7 +9,7 @@ why: A dummy comment to make Jekyll process this file
         "author": {{ article.author | jsonify }},
         "category": {{ article.category | jsonify }},
         "pubDate": {{ article.date | jsonify }},
-        "cover-photo": {{ article.cover-photo | jsonify }},
+        "cover-photo": {{ article.cover-photo | thumb_url: 640 | jsonify }},
         "perex": {{ article.excerpt | strip_html | jsonify }},
         "serial": {{ article.serial | jsonify }},
         "relativeURL": {{ article.url | jsonify }},
