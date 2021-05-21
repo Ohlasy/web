b562812355736e1d33aa44f4f89ca351313dd015
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-05-21 14:50:24 +0200

    Přidal jsem testy API pro Darujme

---
+++ _tests/darujme.test.ts
@@ -0,0 +1,58 @@
+import {
+  Transaction,
+  getRecurrentDonations,
+  getOneTimeDonations,
+  formatDate,
+} from "../api/dary";
+
+test("Date formatting", () => {
+  expect(formatDate(new Date("2020-4-1"))).toBe("2020-04-01");
+});
+
+test("Stats calculation", () => {
+  const txs: Transaction[] = [
+    {
+      transactionId: 1,
+      sentAmount: {
+        cents: 100_00,
+        currency: "CZK",
+      },
+      pledge: {
+        pledgeId: 1,
+        isRecurrent: true,
+      },
+    },
+    {
+      transactionId: 2,
+      sentAmount: {
+        cents: 200_00,
+        currency: "CZK",
+      },
+      pledge: {
+        pledgeId: 2,
+        isRecurrent: true,
+      },
+    },
+    {
+      transactionId: 2391206,
+      sentAmount: null,
+      pledge: {
+        pledgeId: 1863762,
+        isRecurrent: true,
+      },
+    },
+    {
+      transactionId: 3,
+      sentAmount: {
+        cents: 400_00,
+        currency: "CZK",
+      },
+      pledge: {
+        pledgeId: 3,
+        isRecurrent: false,
+      },
+    },
+  ];
+  expect(getRecurrentDonations(txs)).toEqual(300);
+  expect(getOneTimeDonations(txs)).toEqual(400);
+});
