d0756c5dd347245a858db251096807c631c2d06e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-06-21 15:55:22 +0200

    Přidal jsem endpoint pro statistiky počtu textů

---
+++ api/obsah/pocty-textu.ts
@@ -0,0 +1,12 @@
+import { getArticleIndex, send, renderCSV } from "./_shared";
+
+export default send("text/csv", async () => {
+  let stats: Record<string, number> = {};
+  const articles = await getArticleIndex();
+  articles.forEach((article) => {
+    const pubDate = new Date(article.pubDate);
+    const month = `${pubDate.getMonth() + 1}/${pubDate.getFullYear()}`;
+    stats[month] = stats[month] ? stats[month] + 1 : 1;
+  });
+  return renderCSV(stats);
+});
