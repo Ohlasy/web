ec04d684661d583bc4c5488c441845fed1d1fddd
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-07-16 09:30:21 +0200

    Terraform: nastavení CloudFront distribucí pro obrázky

--- infra.tf
+++ infra.tf
@@ -1,3 +1,7 @@
+#
+# General Config
+#
+
 terraform {
   required_providers {
     aws = {
@@ -21,6 +25,10 @@ provider "aws" {
   alias  = "us-east-1"
 }
 
+#
+# Data Storage
+#
+
 resource "aws_s3_bucket" "data" {
   bucket = "data.ohlasy.info"
   acl    = "public-read"
@@ -28,6 +36,61 @@ resource "aws_s3_bucket" "data" {
     index_document = "index.html"
   }
 }
+
+resource "aws_acm_certificate" "data-ohlasy-info" {
+  provider          = aws.us-east-1
+  domain_name       = "data.ohlasy.info"
+  validation_method = "DNS"
+}
+
+resource "aws_cloudfront_distribution" "data-ohlasy-info" {
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+  price_class         = "PriceClass_100"
+
+  aliases = ["data.ohlasy.info"]
+
+  origin {
+    domain_name = aws_s3_bucket.data.bucket_domain_name
+    origin_id   = "S3-data.ohlasy.info"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-data.ohlasy.info"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 86400
+    max_ttl                = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.data-ohlasy-info.arn
+    minimum_protocol_version = "TLSv1.1_2016"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+#
+# Images
+#
 
 resource "aws_s3_bucket" "img" {
   bucket = "i.ohlasy.info"
@@ -40,12 +103,6 @@ resource "aws_s3_bucket" "img" {
 resource "aws_acm_certificate" "i-ohlasy-info" {
   provider          = aws.us-east-1
   domain_name       = "i.ohlasy.info"
-  validation_method = "DNS"
-}
-
-resource "aws_acm_certificate" "data-ohlasy-info" {
-  provider          = aws.us-east-1
-  domain_name       = "data.ohlasy.info"
   validation_method = "DNS"
 }
 
@@ -95,6 +152,56 @@ resource "aws_cloudfront_distribution" "i-ohlasy-info" {
 
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.i-ohlasy-info.arn
+    minimum_protocol_version = "TLSv1.1_2016"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+resource "aws_cloudfront_distribution" "thumbs-cdn-ohlasy-info" {
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_100"
+
+  aliases = ["thumbs-cdn.ohlasy.info"]
+
+  origin {
+    domain_name = "thumbs.ohlasy.info"
+    origin_id   = "thumbs.ohlasy.info"
+    custom_origin_config {
+      http_port              = 8080
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "thumbs.ohlasy.info"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 31536000
+    default_ttl            = 31536000
+    max_ttl                = 31536000
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.thumbs-cdn-ohlasy-info.arn
     minimum_protocol_version = "TLSv1.1_2016"
     ssl_support_method       = "sni-only"
   }
