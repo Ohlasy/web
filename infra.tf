849075e54f779254471c7529436d9526715c555e
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-07-30 18:08:48 +0200

    Terraform: přidal jsem novou obrázkovou keš

--- infra.tf
+++ infra.tf
@@ -20,13 +20,18 @@ provider "aws" {
   region = "eu-central-1"
 }
 
+# We use the us-east-1 region to store SSL certificates
+# (It’s the only region certs can be stored in.)
 provider "aws" {
   region = "us-east-1"
   alias  = "us-east-1"
 }
 
 #
-# Data Storage
+# Data Storage / data.ohlasy.info
+#
+# This is where we store “attachments” or large files we need a nice public URL to.
+# It’s an S3 bucket behind a CloudFront distribution.
 #
 
 resource "aws_s3_bucket" "data" {
@@ -89,7 +94,11 @@ resource "aws_cloudfront_distribution" "data-ohlasy-info" {
 }
 
 #
-# Images
+# Master image store / i.ohlasy.info
+#
+# This is where we store image “masters”, ie. full-resolution photos. An S3 bucket behind
+# a CloudFront distribution. Most of the time we don’t refer to these images directly, but
+# through a rescaling service (see below) that creates appropriate lower-resolution images.
 #
 
 resource "aws_s3_bucket" "img" {
@@ -103,12 +112,6 @@ resource "aws_s3_bucket" "img" {
 resource "aws_acm_certificate" "i-ohlasy-info" {
   provider          = aws.us-east-1
   domain_name       = "i.ohlasy.info"
-  validation_method = "DNS"
-}
-
-resource "aws_acm_certificate" "thumbs-cdn-ohlasy-info" {
-  provider          = aws.us-east-1
-  domain_name       = "thumbs-cdn.ohlasy.info"
   validation_method = "DNS"
 }
 
@@ -157,6 +160,16 @@ resource "aws_cloudfront_distribution" "i-ohlasy-info" {
   }
 }
 
+#
+# Legacy low-res image cache
+#
+
+resource "aws_acm_certificate" "thumbs-cdn-ohlasy-info" {
+  provider          = aws.us-east-1
+  domain_name       = "thumbs-cdn.ohlasy.info"
+  validation_method = "DNS"
+}
+
 resource "aws_cloudfront_distribution" "thumbs-cdn-ohlasy-info" {
   enabled         = true
   is_ipv6_enabled = true
@@ -202,6 +215,67 @@ resource "aws_cloudfront_distribution" "thumbs-cdn-ohlasy-info" {
 
   viewer_certificate {
     acm_certificate_arn      = aws_acm_certificate.thumbs-cdn-ohlasy-info.arn
+    minimum_protocol_version = "TLSv1.1_2016"
+    ssl_support_method       = "sni-only"
+  }
+}
+
+#
+# New low-res image cache
+#
+
+resource "aws_acm_certificate" "nahledy-ohlasy-info" {
+  provider          = aws.us-east-1
+  domain_name       = "nahledy.ohlasy.info"
+  validation_method = "DNS"
+}
+
+resource "aws_cloudfront_distribution" "nahledy-ohlasy-info" {
+  enabled         = true
+  is_ipv6_enabled = true
+  price_class     = "PriceClass_100"
+
+  aliases = ["nahledy.ohlasy.info"]
+
+  origin {
+    domain_name = "ohlasy.info"
+    origin_id   = "ohlasy.info"
+    origin_path = "/api/resize"
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1"]
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "ohlasy.info"
+
+    forwarded_values {
+      query_string = true
+      cookies {
+        forward = "none"
+      }
+    }
+
+    # TBD: Bump the TTL once things are smoothed out
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 60
+    default_ttl            = 60
+    max_ttl                = 60
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn      = aws_acm_certificate.nahledy-ohlasy-info.arn
     minimum_protocol_version = "TLSv1.1_2016"
     ssl_support_method       = "sni-only"
   }
