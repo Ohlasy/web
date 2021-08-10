4ed6ca6879497b96debfdffd48d6615de6fb324c
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2021-08-10 19:42:53 +0200

    Terraform: zahodil jsem původní náhledovou infrastrukturu

--- infra.tf
+++ infra.tf
@@ -161,67 +161,7 @@ resource "aws_cloudfront_distribution" "i-ohlasy-info" {
 }
 
 #
-# Legacy low-res image cache
-#
-
-resource "aws_acm_certificate" "thumbs-cdn-ohlasy-info" {
-  provider          = aws.us-east-1
-  domain_name       = "thumbs-cdn.ohlasy.info"
-  validation_method = "DNS"
-}
-
-resource "aws_cloudfront_distribution" "thumbs-cdn-ohlasy-info" {
-  enabled         = true
-  is_ipv6_enabled = true
-  price_class     = "PriceClass_100"
-
-  aliases = ["thumbs-cdn.ohlasy.info"]
-
-  origin {
-    domain_name = "thumbs.ohlasy.info"
-    origin_id   = "thumbs.ohlasy.info"
-    custom_origin_config {
-      http_port              = 8080
-      https_port             = 443
-      origin_protocol_policy = "http-only"
-      origin_ssl_protocols   = ["TLSv1"]
-    }
-  }
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "thumbs.ohlasy.info"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 31536000
-    default_ttl            = 31536000
-    max_ttl                = 31536000
-  }
-
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
-  viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.thumbs-cdn-ohlasy-info.arn
-    minimum_protocol_version = "TLSv1.1_2016"
-    ssl_support_method       = "sni-only"
-  }
-}
-
-#
-# New low-res image cache
+# Low-res image cache
 #
 
 resource "aws_acm_certificate" "nahledy-ohlasy-info" {
