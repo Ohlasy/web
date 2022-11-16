7b7cc3efdc6313448258426cbfa454d0f9a13e77
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-11-16 13:49:45 +0100

    Extract banner loading code

--- pages/api/bannery.ts
+++ pages/api/bannery.ts
@@ -1,46 +1,13 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { Client } from "@notionhq/client";
-import {
-  CheckboxPropertyValue,
-  NumberPropertyValue,
-  Page,
-  RichTextPropertyValue,
-  TitlePropertyValue,
-  URLPropertyValue,
-} from "@notionhq/client/build/src/api-types";
-
-interface BannerPage extends Page {
-  properties: {
-    "Název"?: TitlePropertyValue;
-    "Odkaz"?: URLPropertyValue;
-    "Zveřejnit"?: CheckboxPropertyValue;
-    "Obrázek"?: URLPropertyValue;
-    "Náhradní text"?: RichTextPropertyValue;
-    "Priorita"?: NumberPropertyValue;
-  };
-}
-
-interface Banner {
-  name: string;
-  image: string;
-  alt: string;
-  url: string;
-  priority: number;
-}
+import { getAllBanners } from "src/banners";
 
 export default async (
   request: NextApiRequest,
   response: NextApiResponse
 ): Promise<void> => {
-  const notion = new Client({ auth: process.env.NOTION_API_KEY });
-  const databaseId = "50c3e92b4f7d44ee9cc5a139d81b07b5";
-
-  const dbResponse = await notion.databases.query({ database_id: databaseId });
-  const results = dbResponse.results
-    .map(bannerFromPage)
-    .filter((b) => b != null);
-
-  const out = JSON.stringify(results, null, 2);
+  const apiKey = process.env.NOTION_API_KEY ?? "<not set>";
+  const banners = await getAllBanners(apiKey);
+  const out = JSON.stringify(banners, null, 2);
   response.setHeader(
     "Cache-Control",
     "max-age=0, s-maxage=60, stale-while-revalidate=86400"
@@ -48,24 +15,3 @@ export default async (
   response.setHeader("Content-Type", "application/json");
   response.status(200).send(out);
 };
-
-function bannerFromPage(page: Page): Banner | null {
-  const props = (page as BannerPage).properties;
-
-  const publish = props["Zveřejnit"]?.checkbox;
-  if (publish !== true) {
-    return null;
-  }
-
-  const name = props["Název"]?.title[0]?.plain_text;
-  const image = props["Obrázek"]?.url;
-  const alt = props["Náhradní text"]?.rich_text[0]?.plain_text;
-  const url = props["Odkaz"]?.url;
-  const priority = props["Priorita"]?.number || 0;
-
-  if (!(name && image && alt && url)) {
-    return null;
-  }
-
-  return { name, image, alt, url, priority };
-}
---
+++ src/banners.ts
@@ -0,0 +1,59 @@
+import { Client } from "@notionhq/client";
+import { notEmpty } from "./utils";
+import {
+  CheckboxPropertyValue,
+  NumberPropertyValue,
+  Page,
+  RichTextPropertyValue,
+  TitlePropertyValue,
+  URLPropertyValue,
+} from "@notionhq/client/build/src/api-types";
+
+interface BannerPage extends Page {
+  properties: {
+    "Název"?: TitlePropertyValue;
+    "Odkaz"?: URLPropertyValue;
+    "Zveřejnit"?: CheckboxPropertyValue;
+    "Obrázek"?: URLPropertyValue;
+    "Náhradní text"?: RichTextPropertyValue;
+    "Priorita"?: NumberPropertyValue;
+  };
+}
+
+export interface Banner {
+  name: string;
+  image: string;
+  alt: string;
+  url: string;
+  priority: number;
+}
+
+export async function getAllBanners(apiKey: string): Promise<Banner[]> {
+  const notion = new Client({ auth: apiKey });
+  const database = "50c3e92b4f7d44ee9cc5a139d81b07b5";
+  const results = await notion.databases
+    .query({ database_id: database })
+    .then((response) => response.results);
+  return results.map(bannerFromPage).filter(notEmpty);
+}
+
+function bannerFromPage(page: Page): Banner | null {
+  const props = (page as BannerPage).properties;
+
+  const publish = props["Zveřejnit"]?.checkbox;
+  if (publish !== true) {
+    return null;
+  }
+
+  const name = props["Název"]?.title[0]?.plain_text;
+  const image = props["Obrázek"]?.url;
+  const alt = props["Náhradní text"]?.rich_text[0]?.plain_text;
+  const url = props["Odkaz"]?.url;
+  const priority = props["Priorita"]?.number || 0;
+
+  if (!(name && image && alt && url)) {
+    return null;
+  }
+
+  return { name, image, alt, url, priority };
+}
