e980f10af073e01d9ffb62c87c39d99a7e46693b
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-11-20 18:24:14 +0100

    Improve title loading hack in analytics API

--- pages/api/analytics.ts
+++ pages/api/analytics.ts
@@ -1,14 +1,8 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { queryTopPages } from "src/data-source/plausible";
-import fetch from "node-fetch";
-
-// TBD: This is a sorry hack, we should get the titles locally or from Plausible.
-async function getPageTitle(url: string) {
-  const response = await fetch(url);
-  const src = await response.text();
-  const matches = src.match(/<title>([^<]*)<\/title>/);
-  return matches ? matches[1] : "";
-}
+import { getAllArticles } from "src/article";
+import { articleRoot } from "src/server-utils";
+import { getArticlePath } from "src/routing";
 
 export default async (
   request: NextApiRequest,
@@ -20,22 +14,21 @@ export default async (
     return;
   }
   const report = await queryTopPages(apiKey);
+  const allArticles = getAllArticles(articleRoot);
+  // TBD: This is very inefficient
+  const getPageTitle = (path: string) =>
+    allArticles.find((a) => getArticlePath(a) === path)?.title;
   const topArticles = report.results
     // We only want regular articles
     .filter((item) => item.page.match("^/clanky"))
     // Convert to expected format
     .map((item) => ({
       url: item.page,
+      title: getPageTitle(item.page),
     }))
     // Only take 10
     .slice(0, 10);
-  const articlesWithTitles = await Promise.all(
-    topArticles.map(async (item) => ({
-      title: await getPageTitle("https://ohlasy.info" + item.url),
-      ...item,
-    }))
-  );
-  const out = JSON.stringify(articlesWithTitles, null, 2);
+  const out = JSON.stringify(topArticles, null, 2);
   response.setHeader("Cache-Control", "max-age=0, s-maxage=86400");
   response.setHeader("Content-Type", "application/json");
   response.status(200).send(out);
