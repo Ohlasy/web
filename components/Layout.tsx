45f441a26dffd2aa39ceda4904bc9aae49a042fb
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2022-11-22 17:48:16 +0100

    Move Bootstrap import to shared stylesheet

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,12 +18,6 @@ export const Layout: React.FC<LayoutProps> = ({ title, children }) => {
         <meta charSet="utf-8" />
         <title>{title}</title>
         <link rel="shortcut icon" type="image/png" href="/favicon.png" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
-          crossOrigin="anonymous"
-        ></link>
       </Head>
       <NavBar />
       {children}
