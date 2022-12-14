85ce334cd5dc61ad5e8cb59ba61ba4bb87d62b49
Tomáš Znamenáček <zoul@zoul.cz>
2022-12-14 15:04:41 +0100

    Fix tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -8,7 +8,7 @@ test("Image resizing", () => {
       "oovah1Ch"
     )
   ).toBe(
-    "https://nahledy.ohlasy.info/?src=https://i.ohlasy.info/i/53173baa.jpg&width=640&proof=73bcf420ab6236793b3fda3bc5a2bf779ad50c13"
+    "/api/resize?src=https://i.ohlasy.info/i/53173baa.jpg&width=640&proof=73bcf420ab6236793b3fda3bc5a2bf779ad50c13"
   );
 });
 
