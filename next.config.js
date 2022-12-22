93e22cd6a8320cf92582b54a1dcbf5993c9847dc
Tomáš Znamenáček <zoul@zoul.cz>
2022-12-22 10:03:40 +0100

    Move redirect to next.config.js, fix trailing slashes

--- next.config.js
+++ next.config.js
@@ -33,6 +33,41 @@ const nextConfig = {
       },
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: "/podcast.html",
+        destination: "/podcast/",
+        permanent: true,
+      },
+      {
+        source: "/go/cisla/",
+        destination:
+          "https://www.notion.so/ohlasy/Ohlasy-v-slech-0cce47372104440485ff3ddeb90705c3",
+        permanent: false,
+      },
+      {
+        source: "/go/petiletka/",
+        destination: "https://data.ohlasy.info/2019/vyrocni-zprava.pdf",
+        permanent: false,
+      },
+      {
+        source: "/go/pripadovka/",
+        destination: "https://data.ohlasy.info/2021/pripadovka.pdf",
+        permanent: false,
+      },
+      {
+        source: "/darujte/",
+        destination: "https://www.darujme.cz/projekt/1202392",
+        permanent: false,
+      },
+      {
+        source: "/darujme/",
+        destination: "https://www.darujme.cz/projekt/1202392",
+        permanent: false,
+      },
+    ];
+  },
   trailingSlash: true,
 };
 
