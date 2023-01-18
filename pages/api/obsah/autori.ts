ff766ad5c18be40476bde90a51f8b42943b4243c
Tomáš Znamenáček <zoul@zoul.cz>
2023-01-18 10:26:49 +0100

    Add year filtering to author stats endpoint

--- pages/api/obsah/autori.ts
+++ pages/api/obsah/autori.ts
@@ -3,12 +3,36 @@ import { articleRoot } from "src/server-utils";
 import {
   getArticlesByAuthor,
   sum,
-  send,
   renderCSV,
 } from "src/data-source/content-stats";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default send("text/csv", async () => {
-  const articles = getAllArticles(articleRoot);
-  const stats = sum(getArticlesByAuthor(articles));
-  return renderCSV(stats);
-});
+export default function handler(
+  request: NextApiRequest,
+  response: NextApiResponse
+) {
+  try {
+    const year =
+      typeof request.query.year === "string"
+        ? parseInt(request.query.year)
+        : undefined;
+    const articles = getAllArticles(articleRoot).filter((a) => {
+      if (year) {
+        const date = new Date(a.date);
+        return date.getFullYear() === year;
+      } else {
+        return true;
+      }
+    });
+    const stats = sum(getArticlesByAuthor(articles));
+    response.setHeader(
+      "Cache-Control",
+      `s-maxage=3600, stale-while-revalidate`
+    );
+    response.setHeader("Access-Control-Allow-Origin", "*");
+    response.setHeader("Content-Type", "text/csv");
+    response.status(200).send(renderCSV(stats));
+  } catch {
+    response.status(500).send("Error loading source data.");
+  }
+}
