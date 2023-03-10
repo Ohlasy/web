bf28d34349d635d56c2be6045f18219ae87c33f2
Tomáš Znamenáček <zoul@zoul.cz>
2023-03-10 10:15:24 +0100

    Move to modern web components Datawrapper embeds

--- app/clanky/[...path]/DatawrapperChart.tsx
+++ app/clanky/[...path]/DatawrapperChart.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-// @ts-ignore, TBD: fix later, types are missing
-import DWChart from "react-datawrapper-chart";
+import { useEffect, useRef } from "react";
 
 type Props = {
   id: string;
@@ -12,11 +11,20 @@ type Props = {
 export const DatawrapperChart = ({
   id,
   version = 1,
-  title = "Graf",
-}: Props) => (
-  <DWChart
-    title={title}
-    src={`https://datawrapper.dwcdn.net/${id}/${version}`}
-    loading="lazy"
-  />
-);
+  title = "Graf", // TBD: insert to embed
+}: Props) => {
+  const elem = useRef<HTMLDivElement | null>(null);
+  const script = useRef<HTMLScriptElement>();
+  useEffect(() => {
+    if (!script.current) {
+      script.current = document.createElement("script");
+      script.current.src = `https://datawrapper.dwcdn.net/${id}/embed.js?v=${version}`;
+      script.current.defer = true;
+      elem.current!.appendChild(script.current);
+    }
+  }, [elem, script, id, version]);
+
+  return (
+    <div ref={elem} style={{ marginTop: "30px", marginBottom: "30px" }}></div>
+  );
+};
