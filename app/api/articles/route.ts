29933574e29f1bee176bedcabca4e366893172da
Tomáš Znamenáček <zoul@zoul.cz>
2023-03-11 17:33:44 +0100

    Move article API endpoint to app router

--- app/api/articles/route.ts
+++ app/api/articles/route.ts
@@ -1,21 +1,19 @@
-import { NextApiRequest, NextApiResponse } from "next";
 import { Article, compareByDate, getAllArticles, stripBody } from "src/article";
 import { getArticlePath } from "src/routing";
 import { articleRoot } from "src/server-utils";
 import { getSignedResizedImage, IMAGE_SIGNING_KEY } from "src/utils";
 
-export default async (
-  request: NextApiRequest,
-  response: NextApiResponse
-): Promise<void> => {
+export async function GET() {
   const articles = getAllArticles(articleRoot)
     .sort(compareByDate)
     .map(addAuxiliaryData);
-  const json = JSON.stringify(articles, null, 2);
-  response.setHeader("Content-Type", "application/json");
-  response.setHeader("Cache-Control", `s-maxage=3600, stale-while-revalidate`);
-  response.send(json);
-};
+  return new Response(JSON.stringify(articles, null, 2), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
 
 function addAuxiliaryData(article: Article) {
   return {
