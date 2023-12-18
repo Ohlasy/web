59d18f51f2b0e88e0977bd5fb890e9b02f5cc28f
Tomáš Znamenáček <zoul@zoul.cz>
2023-12-18 10:41:16 +0100

    Use latest Datawrapper chart version unless specified otherwise

--- app/clanky/[...path]/DatawrapperChart.tsx
+++ app/clanky/[...path]/DatawrapperChart.tsx
@@ -5,24 +5,23 @@ import { useEffect, useRef } from "react";
 type Props = {
   id: string;
   version?: number;
-  title?: string;
+  title?: string; // TBD: insert to embed
 };
 
-export const DatawrapperChart = ({
-  id,
-  version = 1,
-  title = "Graf", // TBD: insert to embed
-}: Props) => {
+export const DatawrapperChart = ({ id, version }: Props) => {
   const elem = useRef<HTMLDivElement | null>(null);
   const script = useRef<HTMLScriptElement>();
+  const embedUrl = version
+    ? `https://datawrapper.dwcdn.net/${id}/embed.js?v=${version}`
+    : `https://datawrapper.dwcdn.net/${id}/embed.js`;
   useEffect(() => {
     if (!script.current) {
       script.current = document.createElement("script");
-      script.current.src = `https://datawrapper.dwcdn.net/${id}/embed.js?v=${version}`;
+      script.current.src = embedUrl;
       script.current.defer = true;
       elem.current!.appendChild(script.current);
     }
-  }, [elem, script, id, version]);
+  }, [elem, script, id, embedUrl]);
 
   return (
     <div ref={elem} style={{ marginTop: "30px", marginBottom: "30px" }}></div>
