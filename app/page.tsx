f1506a76721889bade5b2fa99edc4b61cd3bada3
Tomáš Znamenáček <zoul@zoul.cz>
2024-01-01 11:38:16 +0100

    Sundal jsem vánoční fundraiser
    This reverts commit 68fa7929b815f6359207ed3c2df16d4242e6dd1a.

--- app/page.tsx
+++ app/page.tsx
@@ -54,9 +54,6 @@ const Page = async () => {
         analyticsId="latest-articles-box"
       />
 
-      <h2 className="section-divider">podpořte nás</h2>
-      <FundraisingSecion />
-
       <h2 className="section-divider">názory &amp; komentáře</h2>
       <PreviewNest
         articles={opinions}
@@ -80,6 +77,9 @@ const Page = async () => {
         getBanner={getNextBanner}
         analyticsId="podcast-box"
       />
+
+      <h2 className="section-divider">podpořte nás</h2>
+      <FundraisingSecion />
 
       <h2 className="section-divider">rozhovory</h2>
       <PreviewNest
@@ -116,16 +116,14 @@ const Page = async () => {
 
 const FundraisingSecion = () => (
   <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-7">
-    <div className="lg:col-span-2">
-      <iframe
-        width="100%"
-        height="100%"
-        className="aspect-video"
-        src={`https://www.youtube-nocookie.com/embed/xvg4h0EP_qM`}
-        title="Přispějte nám na provoz"
-        allowFullScreen
-      ></iframe>
-    </div>
+    <Image
+      className="lg:col-span-2"
+      src="https://i.ohlasy.info/i/9b34a8e0.jpg"
+      sizes="(min-width: 640px) 50vw, 100vw"
+      width={3776}
+      height={2517}
+      alt="Tým Ohlasů"
+    />
     <div>
       <iframe
         width="100%"
