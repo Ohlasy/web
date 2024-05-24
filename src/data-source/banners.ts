b0ee11f1cddf97cba9d8156c9f26632ecbdd3645
Tomáš Znamenáček <zoul@zoul.cz>
2024-05-24 10:04:57 +0200

    Improved typed Notion database queries

--- src/data-source/banners.ts
+++ src/data-source/banners.ts
@@ -1,92 +1,25 @@
-import { Client } from "@notionhq/client";
 import { shuffled } from "src/utils";
+import { decodeType, field, record } from "typescript-json-decoder";
 import {
-  array,
-  boolean,
-  field,
-  literal,
-  record,
-  string,
-} from "typescript-json-decoder";
+  decodeCheckbox,
+  decodePlainRichtext,
+  decodeTitle,
+  decodeUrl,
+  getTypedDatabaseRows,
+} from "./notion";
 
-export interface Banner {
-  name: string;
-  image: string;
-  alt: string;
-  url: string;
-}
+export type Banner = decodeType<typeof decodeBannerProps>;
 
-const decodeUrlProperty = record({
-  type: literal("url"),
-  value: field("url", string),
+export const decodeBannerProps = record({
+  name: field("Název", decodeTitle),
+  published: field("Zveřejnit", decodeCheckbox),
+  image: field("Obrázek", decodeUrl),
+  alt: field("Náhradní text", decodePlainRichtext),
+  url: field("Odkaz", decodeUrl),
 });
 
-const decodeCheckboxProperty = record({
-  type: literal("checkbox"),
-  value: field("checkbox", boolean),
-});
-
-const decodeRichTextProperty = record({
-  type: literal("rich_text"),
-  value: field(
-    "rich_text",
-    array(
-      record({
-        type: literal("text"),
-        plainText: field("plain_text", string),
-      })
-    )
-  ),
-});
-
-const decodeTitleProperty = record({
-  type: literal("title"),
-  value: field(
-    "title",
-    array(
-      record({
-        type: literal("text"),
-        plainText: field("plain_text", string),
-      })
-    )
-  ),
-});
-
-const decodeQueryResponse = record({
-  object: literal("list"),
-  results: array(
-    record({
-      object: literal("page"),
-      props: field(
-        "properties",
-        record({
-          url: field("Odkaz", decodeUrlProperty),
-          published: field("Zveřejnit", decodeCheckboxProperty),
-          imageUrl: field("Obrázek", decodeUrlProperty),
-          alt: field("Náhradní text", decodeRichTextProperty),
-          name: field("Název", decodeTitleProperty),
-        })
-      ),
-    })
-  ),
-});
-
-export async function getAllBanners(
-  apiKey: string = process.env.NOTION_API_KEY ?? ""
-): Promise<Banner[]> {
-  const notion = new Client({ auth: apiKey });
-  const database_id = "50c3e92b4f7d44ee9cc5a139d81b07b5";
-  const rows = await notion.databases
-    .query({ database_id })
-    .then(decodeQueryResponse)
-    .then((response) =>
-      response.results.filter((ad) => ad.props.published.value)
-    );
-  const banners = rows.map(({ props }) => ({
-    name: props.name.value[0].plainText,
-    image: props.imageUrl.value,
-    alt: props.alt.value[0].plainText,
-    url: props.url.value,
-  }));
-  return shuffled(banners);
+export async function getAllBanners(): Promise<Banner[]> {
+  const databaseId = "50c3e92b4f7d44ee9cc5a139d81b07b5";
+  const banners = await getTypedDatabaseRows(databaseId, decodeBannerProps);
+  return shuffled(banners.filter((b) => b.published));
 }
---
+++ src/data-source/notion.ts
@@ -0,0 +1,107 @@
+import { Client } from "@notionhq/client";
+import {
+  DecoderFunction,
+  array,
+  boolean,
+  field,
+  literal,
+  record,
+  string,
+} from "typescript-json-decoder";
+
+//
+// Primitive decoders
+//
+
+const notionUrl = record({
+  type: literal("url"),
+  value: field("url", string),
+});
+
+const notionCheckbox = record({
+  type: literal("checkbox"),
+  value: field("checkbox", boolean),
+});
+
+const notionRichText = record({
+  type: literal("rich_text"),
+  value: field(
+    "rich_text",
+    array(
+      record({
+        type: literal("text"),
+        plainText: field("plain_text", string),
+      })
+    )
+  ),
+});
+
+const notionTitle = record({
+  type: literal("title"),
+  value: field(
+    "title",
+    array(
+      record({
+        type: literal("text"),
+        plainText: field("plain_text", string),
+      })
+    )
+  ),
+});
+
+//
+// Value Decoders
+//
+
+const decodeValue = <T>(decoder: DecoderFunction<{ value: T }>) =>
+  map(decoder, (v) => v.value);
+
+export const decodeUrl = decodeValue(notionUrl);
+export const decodeCheckbox = decodeValue(notionCheckbox);
+
+export const decodeTitle = map(notionTitle, (v) =>
+  string(v.value.at(0)?.plainText)
+);
+
+export const decodePlainRichtext = map(notionRichText, (v) =>
+  string(v.value.at(0)?.plainText)
+);
+
+//
+// Support
+//
+
+function map<T, U>(
+  decoder: DecoderFunction<T>,
+  f: (_: T) => U
+): DecoderFunction<U> {
+  return (input: unknown) => f(decoder(input));
+}
+
+export const decodePage = <Props>(decodeProps: DecoderFunction<Props>) =>
+  record({
+    id: string,
+    object: literal("page"),
+    props: field("properties", decodeProps),
+  });
+
+//
+// DB Interface
+//
+
+export async function getTypedDatabaseRows<T>(
+  database_id: string,
+  decodeRow: DecoderFunction<T>,
+  apiKey = process.env.NOTION_API_KEY
+): Promise<T[]> {
+  const decodeResponse = record({
+    object: literal("list"),
+    results: array(decodePage(decodeRow)),
+  });
+  const notion = new Client({ auth: apiKey });
+  const response = await notion.databases.query({ database_id });
+  const results = decodeResponse(response) as any as {
+    results: { props: T }[];
+  };
+  return results.results.map((r) => r.props);
+}
