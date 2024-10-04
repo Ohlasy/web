573834cef3512c95b8b6c436812a8a48fea9acea
Tomáš Znamenáček <zoul@zoul.cz>
2024-10-04 16:42:25 +0200

    Strip article bodies from archive index

--- app/archiv/filters.ts
+++ app/archiv/filters.ts
@@ -1,4 +1,4 @@
-import { Article } from "src/article";
+import { Metadata } from "src/article";
 import { unique } from "src/utils";
 
 export type FilterOptions = Record<string, string[]>;
@@ -8,8 +8,8 @@ export interface Filter {
   id: string;
   name: string;
   valueSort: (a: string, b: string) => number;
-  extractPossibleValues: (article: Article) => string[];
-  match: (article: Article, value: string) => boolean;
+  extractPossibleValues: (article: Metadata) => string[];
+  match: (article: Metadata, value: string) => boolean;
   displayValue?: (value: string) => string;
 }
 
@@ -70,7 +70,7 @@ export const filters: Filter[] = [
   },
 ];
 
-export function buildFilterOptions(articles: Article[]): FilterOptions {
+export function buildFilterOptions(articles: Metadata[]): FilterOptions {
   const getPossibleValues = (f: Filter) =>
     unique(articles.map(f.extractPossibleValues).flat()).sort(f.valueSort);
   const filtersAndValues = filters.map((filter) => [
@@ -80,7 +80,7 @@ export function buildFilterOptions(articles: Article[]): FilterOptions {
   return Object.fromEntries(filtersAndValues);
 }
 
-export function match(article: Article, settings: Settings): boolean {
+export function match(article: Metadata, settings: Settings): boolean {
   for (const [filterId, wantedValue] of Object.entries(settings)) {
     const filter = filters.find((f) => f.id === filterId);
     if (!filter) {
@@ -94,4 +94,4 @@ export function match(article: Article, settings: Settings): boolean {
   return true;
 }
 
-const getYear = (a: Article) => new Date(a.date).getFullYear().toString();
+const getYear = (a: Metadata) => new Date(a.date).getFullYear().toString();
