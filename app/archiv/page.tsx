466990b5c2ec7c3ea2471f5bb71609e60893380f
Tomáš Znamenáček <zoul@zoul.cz>
2024-10-04 16:52:17 +0200

    Add trivial loading suspense for /archiv

--- app/archiv/page.tsx
+++ app/archiv/page.tsx
@@ -1,11 +1,16 @@
 import { getAllArticles, stripBody } from "src/article";
 import { ArchiveView } from "./ArchiveView";
 import { buildFilterOptions } from "./filters";
+import { Suspense } from "react";
 
 async function Page() {
   const articles = getAllArticles("content/articles").map(stripBody);
   const filterOptions = buildFilterOptions(articles);
-  return <ArchiveView allArticles={articles} filterOptions={filterOptions} />;
+  return (
+    <Suspense fallback="Nahrávám…">
+      <ArchiveView allArticles={articles} filterOptions={filterOptions} />
+    </Suspense>
+  );
 }
 
 export default Page;
