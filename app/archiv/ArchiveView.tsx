efb2b6f445cf893286bd77f5ad6ca7ce42050b1e
Tomáš Znamenáček <zoul@zoul.cz>
2024-10-04 16:17:01 +0200

    Add basic filtering UI

--- app/archiv/ArchiveView.tsx
+++ app/archiv/ArchiveView.tsx
@@ -1,19 +1,22 @@
 "use client";
 
 import { Article } from "src/article";
-import { FilterOptions, match } from "./filters";
+import { Filter, FilterOptions, filters, match, Settings } from "./filters";
 import Link from "next/link";
 import Image from "next/image";
 import { RouteTo } from "src/routing";
 import { tilde } from "src/utils";
-import { useSearchParams } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
+import { ChangeEvent } from "react";
 
 type Props = {
   allArticles: ReadonlyArray<Article>;
   filterOptions: FilterOptions;
 };
 
-export const ArchiveView = ({ allArticles }: Props) => {
+export const ArchiveView = ({ allArticles, filterOptions }: Props) => {
+  const router = useRouter();
+
   // Read initial filter settings from URL search params
   const searchParams = useSearchParams() ?? [];
   const settings = Object.fromEntries(searchParams.entries());
@@ -23,11 +26,109 @@ export const ArchiveView = ({ allArticles }: Props) => {
     .filter((a) => match(a, settings))
     .sort(compareByDate);
 
+  // Update filter settings by changing the URL
+  const setSettings = (settings: Record<string, string>) =>
+    router.replace("/archiv/?" + new URLSearchParams(settings));
+
+  // Update settings based on events from the filter panel
+  const updateSettings = (id: string, newValue: string | undefined) => {
+    if (newValue) {
+      setSettings({ ...settings, [id]: newValue });
+    } else {
+      const { [id]: _, ...newSettings } = settings;
+      setSettings(newSettings);
+    }
+  };
+
+  const clearSettings = () => setSettings({});
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
-      {matchingArticles.map((a) => (
-        <ArticlePreview key={a.date + a.title} article={a} />
+    <div className="flex flex-col gap-7">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+        <FilterPanel
+          options={filterOptions}
+          settings={settings}
+          onChange={updateSettings}
+          removeAllFilters={clearSettings}
+        />
+        {matchingArticles.map((a) => (
+          <ArticlePreview key={a.date + a.title} article={a} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+type FilterPanelProps = {
+  options: FilterOptions;
+  settings: Settings;
+  onChange: (id: string, value: string | undefined) => void;
+  removeAllFilters: () => void;
+};
+
+const FilterPanel = ({
+  options,
+  settings,
+  onChange,
+  removeAllFilters,
+}: FilterPanelProps) => {
+  return (
+    <div className="flex flex-col gap-2 bg-lightGray p-4">
+      {filters.map((filter) => (
+        <FilterControl
+          key={filter.id}
+          filter={filter}
+          values={options[filter.id]}
+          selected={settings[filter.id]}
+          onChange={(value) => onChange(filter.id, value)}
+        />
       ))}
+      <div>
+        <button
+          onClick={removeAllFilters}
+          disabled={Object.keys(settings).length === 0}
+        >
+          Smazat filtry
+        </button>
+      </div>
+    </div>
+  );
+};
+
+type FilterControlProps = {
+  filter: Filter;
+  values: string[];
+  selected: string | undefined;
+  onChange: (value: string | undefined) => void;
+};
+
+export const FilterControl = (props: FilterControlProps) => {
+  const emptyLabel = "bez omezení";
+  const { filter, values, selected, onChange } = props;
+  const handleChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    const reportedValue = value === emptyLabel ? undefined : value;
+    onChange(reportedValue);
+  };
+  const identity = (a: string) => a;
+  const dressValue = filter.displayValue || identity;
+  return (
+    <div>
+      <div>
+        {filter.name} {selected && <span className="text-blue-400">●</span>}
+      </div>
+      <select
+        className="w-full"
+        onChange={handleChange}
+        value={selected ?? emptyLabel}
+      >
+        <option key="na">{emptyLabel}</option>
+        {values.map((item, index) => (
+          <option key={index} value={item}>
+            {dressValue(item)}
+          </option>
+        ))}
+      </select>
     </div>
   );
 };
