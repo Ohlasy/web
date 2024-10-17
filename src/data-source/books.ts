f9e470cb9f9991ad67f05f452a622a8e446770e9
Tomáš Znamenáček <zoul@zoul.cz>
2024-10-17 15:52:23 +0200

    Load book data from Airtable instead of Notion

--- src/data-source/books.ts
+++ src/data-source/books.ts
@@ -1,27 +1,61 @@
-import { decodeType, field, record } from "typescript-json-decoder";
+import Airtable, { FieldSet, Record, Records } from "airtable";
 import {
-  decodeId,
-  decodePlainRichtext,
-  decodeTitle,
-  decodeUrl,
-  getTypedDatabaseRows,
-} from "./notion";
+  array,
+  decodeType,
+  field,
+  number,
+  record,
+  string,
+} from "typescript-json-decoder";
 
-export type Book = decodeType<typeof decodeBookProps>;
+//
+// Airtable Support
+//
 
-export const decodeBookProps = record({
-  id: field("ID", decodeId),
-  title: field("Název", decodeTitle),
-  slug: field("URL slug", decodePlainRichtext),
-  subtitle: field("Podtitul", decodePlainRichtext),
-  description: field("Anotace", decodePlainRichtext),
-  coverImageUrl: field("Fotka", decodeUrl),
-  publishYear: field("Rok vydání", decodePlainRichtext),
-  authors: field("Autorstvo", decodePlainRichtext),
+/** Unwrap the raw fields object from an Airtable `Record` type */
+export const unwrapRecord = <Schema extends FieldSet>(record: Record<Schema>) =>
+  record.fields as unknown;
+
+/** Unwrap the raw fields objects from an Airtable `Records` type */
+export const unwrapRecords = <Schema extends FieldSet>(
+  records: Records<Schema>
+) => records.map(unwrapRecord);
+
+const getTable = (tableNameOrId: string) =>
+  new Airtable().base("apptZroU7Vo0zMDjT")(tableNameOrId);
+
+const titleTableId = "tblODWl1pWKSEAKmv";
+
+//
+// API Types
+//
+
+export type Book = decodeType<typeof decodeBook>;
+export const decodeBook = record({
+  id: field("ID", number),
+  title: field("Název", string),
+  slug: field("URL slug", string),
+  subtitle: field("Podtitul", string),
+  description: field("Anotace", string),
+  coverImageUrl: field("Fotka", string),
+  publishYear: field("Rok vydání", string),
+  authors: field("Autorstvo", string),
 });
 
+//
+// API Calls
+//
+
 export const getAllBooks = async () =>
-  getTypedDatabaseRows("c0bf61afc9d7441c9ab67f1b7e917899", decodeBookProps);
+  getTable(titleTableId)
+    .select()
+    .all()
+    .then(unwrapRecords)
+    .then(array(decodeBook));
+
+//
+// Helpers
+//
 
 export const sortByYear = (a: Book, b: Book) =>
   b.publishYear.localeCompare(a.publishYear);
