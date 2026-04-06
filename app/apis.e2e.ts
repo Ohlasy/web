3e5c85c79a862b9e270f1b8305b2074a743e1126
Tomáš Znamenáček <zoul@zoul.cz>
2026-04-06 11:14:50 +0200

    Remove obsolete /api/articles endpoint

--- app/apis.e2e.ts
+++ app/apis.e2e.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 const ContentType = {
   rss: "application/rss+xml",
@@ -9,7 +9,6 @@ const endpoints = {
   "/feed.xml": ContentType.rss,
   "/podcast.xml": ContentType.rss,
   "/podcast/hrebenovka.xml": ContentType.rss,
-  "/api/articles": ContentType.json,
 };
 
 for (const [endpoint, contentType] of Object.entries(endpoints)) {
