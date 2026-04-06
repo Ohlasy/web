8fa3b6b393a65f930c943362c62d0c00e546dedd
Tomáš Znamenáček <zoul@zoul.cz>
2026-04-06 11:36:16 +0200

    Remove obsolete app wrapper

--- app/_app.js
+++
@@ -1,9 +0,0 @@
-import { Provider as TitleBalanceProvider } from "react-wrap-balancer";
-
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <TitleBalanceProvider>
-      <Component {...pageProps} />
-    </TitleBalanceProvider>
-  );
-}
