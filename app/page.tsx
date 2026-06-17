2a8ccf9b54a729ef449f697d5a1592131953db2f
Tomáš Znamenáček <zoul@zoul.cz>
2026-06-17 19:49:19 +0200

    Try disabling banner shuffle

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export const revalidate = 300;
 
 /** Home page */
 const Page = async () => {
-  const banners = await getAllBanners().then(shuffleInPlace);
+  const banners = await getAllBanners();
   const topArticles = await getTopArticles();
   const latestForumSummary = await getLatestTopicsSummary();
   const articles = getAllArticles("content/articles").sort(compareByDate);
